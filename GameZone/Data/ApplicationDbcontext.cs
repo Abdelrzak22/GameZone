41b784e201b61aeabc1ef7cae7bdadec2f57e0a0
Abdelrazzaq <abdotaha12345m@gmail.com>
2026-02-10 20:56:12 +0200

    seed Defualt Categories and Devices

--- GameZone/Data/ApplicationDbcontext.cs
+++ GameZone/Data/ApplicationDbcontext.cs
@@ -13,6 +13,31 @@ namespace GameZone.Data
         {
             modelBuilder.Entity<GameDevice>()
                 .HasKey(p => new { p.GameId, p.DeviceId });
+
+
+            modelBuilder.Entity<Categorey>()
+                .HasData(new Categorey[] {
+
+                    new Categorey{ Id=1, Name="Action" },
+                    new Categorey{ Id=2, Name="Sports" },
+                    new Categorey{ Id=3, Name="Fighting" },
+                    new Categorey{ Id=4, Name="Film" },
+                    new Categorey{ Id=5, Name="Adventure" },
+                    new Categorey{ Id=6, Name="Racing" },
+                });
+
+
+            modelBuilder.Entity<Device>()
+                .HasData(new Device[] {
+
+
+                    new Device{ Id=1,Name="Playstation",Icon="bi bi-playstation" },
+                    new Device{ Id=2,Name="xbox",Icon="bi bi-xbox" },
+                    new Device{Id=3 ,Name="Nintendo switch",Icon="bi bi-nintendo-switch"},
+                    new Device{ Id=4 ,Name="Pc",Icon="bi bi-pc-display"}
+
+                });
+                
         }
         public DbSet<Game> Games { get; set; }
         public DbSet<Device> Devices { get; set; }
