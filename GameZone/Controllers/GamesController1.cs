cdced5f495e428a9c79e43d62bae4af2dc3dc5f3
Abdelrazzaq <abdotaha12345m@gmail.com>
2026-02-14 13:54:19 +0200

    Add creat game post Action and server side validations

--- GameZone/Controllers/GamesController1.cs
+++ GameZone/Controllers/GamesController1.cs
@@ -35,6 +35,27 @@ namespace GameZone.Controllers
             return View(viewmodel);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CreateGamefromviewmodel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+
+                model.Categories = _context.Categories.Select(c => new SelectListItem { Value = c.Id.ToString(), Text = c.Name }).OrderBy(c => c.Text).ToList();
+                model.Devices = _context.Categories.Select(d => new SelectListItem { Value = d.Id.ToString(), Text = d.Name }).OrderBy(x => x.Text).ToList();
+                return View(model);
+
+            }
+            //save game to database
+            // save cover to server
+
+            return RedirectToAction(nameof(Index));
+
+       
+        }
+
 
     }
 }
