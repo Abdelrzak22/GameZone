582195be1435495f1fd8eb2c39447194199043db
Abdelrazzaq <abdotaha12345m@gmail.com>
2026-02-20 14:34:21 +0200

    Add cover preview

--- GameZone/Setting/Filesetting.cs
+++ GameZone/Setting/Filesetting.cs
@@ -3,7 +3,7 @@
     public abstract class Filesetting
     {
         public const string ImagePath = "/assests/image/games";
-        public const string Allowedextentions = ".jpg,.png,.jpeg";
+        public const string Allowedextentions = ".jpg,.png,.jpng";
         public const int maxfilesizeMB = 1;
         public const int maxfilesizebyte = maxfilesizeMB * 1024 * 1024;
 
